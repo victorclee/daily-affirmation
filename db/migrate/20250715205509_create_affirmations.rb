6350d9036975542c2e6f0343e04395919c229042
Victor Lee <victorchristopherlee@gmail.com>
2025-07-15 15:57:38 -0500

    Creates Affirmation model and migration
    Sets up the Affirmation model with url and translation attributes.
    
    Generates the corresponding database migration file to create the
    affirmations table, and adds basic fixture and test files for the new model.

---
+++ db/migrate/20250715205509_create_affirmations.rb
@@ -0,0 +1,10 @@
+class CreateAffirmations < ActiveRecord::Migration[8.0]
+  def change
+    create_table :affirmations do |t|
+      t.string :url
+      t.string :translation
+
+      t.timestamps
+    end
+  end
+end
